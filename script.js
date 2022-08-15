f173b7aef2060f41152a201fc9805b723ef4031f
Edward <Ecampbell3015@gmail.com>
2022-08-14 18:33:50 -0700

    add header search drop down

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const headerFindDealerSection = document.querySelector('.header__find-dealer-section');
 const headerSearchSection = document.querySelector('.header__search-section');
-const headerSearchXIcon
+const headerSearchXIcon = document.querySelector('.header__search-x-icon');
+const headerSearchIcon = document.querySelector('.header__search-icon');
+
+
+
+
+/* header - header__search-section-open
+x-icon - header__search-open-x-icon
+search-icon - header__search-open-icon */
