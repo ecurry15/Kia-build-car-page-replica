d45f4b4f2e10281d248b1cc55550ba11a4a68c3a
Edward <Ecampbell3015@gmail.com>
2022-08-14 18:03:00 -0700

    setup html for header search bar animation

---
+++ script.js
@@ -0,0 +1,3 @@
+const headerFindDealerSection = document.querySelector('.header__find-dealer-section');
+const headerSearchSection = document.querySelector('.header__search-section');
+const headerSearchXIcon
